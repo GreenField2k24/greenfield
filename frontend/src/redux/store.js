06c5290bf02fcacdabb20e5796127f9f41a706cc
Khalfa Aymen <khalfaaymen141@gmail.com>
2024-09-11 22:58:36 +0100

    redux update

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,1 +1,11 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './action'; // Ensure this path is correct
+
+const store = configureStore({
+  reducer: {
     
+  },
+});
+
+export default store;
